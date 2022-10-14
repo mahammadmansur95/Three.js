b2028335a421c967147eef829ac4f6e1763699b9
mahammad_mansur <mansuralimahammad@gmail.com>
2022-10-14 18:15:57 +0530

    added animation to the box using tick() function

--- Module/src/script.js
+++ Module/src/script.js
@@ -1,69 +1,51 @@
 import './style.css';
 import * as THREE from 'three';
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl');
-
-// Scene
+//scene
 const scene = new THREE.Scene();
 
-/**
- * Objects
- */
-const group = new THREE.Group();
-group.position.y = 1;
-group.scale.y = 2;
-group.rotation.y = 1;
-scene.add(group);
+//Red cube(object = geometry + material)
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
 
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 0xff0000})
-);
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 0x00ff00})
-);
-
-cube2.position.x = -2;
-
-group.add(cube1);
-group.add(cube2);
-
-
-/* Axis Healper */
-const axisHelper = new THREE.AxesHelper();
-scene.add(axisHelper);
-
-
-/**
- * Sizes
- */
+//sizes
 const sizes = {
     width: 800,
     height: 600
-}
+};
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+//camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 // camera.position.x = 1;
 // camera.position.y = 1;
 scene.add(camera);
 
-// console.log('distance between camera and cube ',mesh.position.distanceTo(camera.position));
-
-// You can normalize its values (meaning that you will reduce the length of the vector to 1 unit but preserve its direction)
-// mesh.position.normalize();
-
-/**
- * Renderer
- */
+//Render
+const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+    canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+
+let time = Date.now();
+
+// Animation
+const tick = () => {
+
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+    time = currentTime;
+
+    // Update Object
+    mesh.rotation.y += 0.001 * deltaTime;
+
+    // Render
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+};
+
+tick();
