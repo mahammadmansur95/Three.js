532f3830f9db46179a6da104256f8863e41e800a
mahammad_mansur <mansuralimahammad@gmail.com>
2022-10-17 22:30:39 +0530

    best example to turn the head of the object while we move mouse

--- Module/src/script.js
+++ Module/src/script.js
@@ -1,47 +1,70 @@
-import './style.css';
-import * as THREE from 'three';
-import gsap from 'gsap';
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
+
+
+// Cursor
+const cursor = {
+    x : 0,
+    y: 0,
+};
+window.addEventListener('mousemove', (e) => {
+    cursor.x = e.clientX / sizes.width - 0.5
+    cursor.y = -(e.clientY / sizes.height - 0.5)
+});
 
 //scene
 const scene = new THREE.Scene();
 
 //Red cube(object = geometry + material)
-const geometry = new THREE.BoxGeometry(1,1,1);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry,material);
-scene.add(mesh);
+const mesh = new THREE.Mesh(geometry, material);
+const axis = new THREE.AxesHelper();
+scene.add(mesh,axis);
 
 //sizes
 const sizes = {
-    width: 800,
-    height: 600
+  width: 800,
+  height: 600,
 };
 
 //camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// const aspectRation = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRation,
+//   1 * aspectRation,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
 camera.position.z = 3;
 // camera.position.x = 1;
 // camera.position.y = 1;
 scene.add(camera);
 
 //Render
-const canvas = document.querySelector('.webgl');
+const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({
-    canvas
+  canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 
-let time = Date.now();
-
-gsap.to(mesh.position, {x : 2, duration: 1, delay : 1});
-gsap.to(mesh.position, {x : 0, duration: 1, delay : 2});
-
 // Animation
 const tick = () => {
-    // Render
-    renderer.render(scene, camera);
+  // Render
+  renderer.render(scene, camera);
 
-    window.requestAnimationFrame(tick);
+  //update camera
+  camera.position.x = cursor.x * 3
+  camera.position.y = cursor.y * 3
+  camera.lookAt(mesh.position);
+
+//   mesh.rotation.y += 0.1;
+
+  window.requestAnimationFrame(tick);
 };
 
 tick();
